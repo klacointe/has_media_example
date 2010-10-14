0a8c482c64db708c9573d9311ac0200040f8e0e9
klacointe <kevinlacointe@gmail.com>
2010-10-14 11:19:37 +0200

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HasMediaExample::Application.config.secret_token = '07019d5e91cfa16d302e43a0de8be8392e1dad2b9ceb747b4f27780de0b669085e333e79d8e6d5e939632cbf5a43b4efdc433baeb450b635dc06627b1c9e8833'
