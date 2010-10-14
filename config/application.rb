60c5adbad7ce528cad9569265f0fbc3694ec01fd
klacointe <kevinlacointe@gmail.com>
2010-10-14 18:03:20 +0200

    add has_media

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,10 @@ module HasMediaExample
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Generators config
+    config.generators do |g|
+      g.orm     :active_record
+    end
   end
 end
